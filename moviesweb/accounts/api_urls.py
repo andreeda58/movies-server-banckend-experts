ebec8597287df7c2edeef2aca1885ef8e74610f5
andreeda58 <70017796+andreeda58@users.noreply.github.com>
2025-09-20 15:56:19 +0300

    aws db configuration with RDS and S3

--- moviesweb/accounts/api_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .api_views import CustomAuthToken, register_user, logout_user
-
-urlpatterns = [
-    path('login/',    CustomAuthToken.as_view(), name='login'),
-    path('register/', register_user,             name='register'),
-    path('logout/',   logout_user,               name='logout'),
-]
